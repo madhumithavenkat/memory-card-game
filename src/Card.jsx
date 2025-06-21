74aeb5867f03a403caf1476122a90c7e11cfbf8d
Madhu <maddyvenkat04@gmail.com>
2025-06-21 14:53:12 -0400

    added proper styling

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,10 +1,11 @@
 export default function Card({ card, onClick, isFlipped, isMatched }) {
   return (
-    <div
-      className="w-24 h-32 m-2 perspective"
-      onClick={onClick}
-    >
-      <div className={`relative w-full h-full transition-transform duration-500 transform-style-preserve-3d ${isFlipped || isMatched ? "rotate-y-180" : ""}`}>
+    <div className="w-24 h-32 m-2 perspective" onClick={onClick}>
+      <div
+        className={`relative w-full h-full duration-500 transition-transform transform-style-preserve-3d ${
+          isFlipped || isMatched ? "rotate-y-180" : ""
+        }`}
+      >
         <div className="absolute w-full h-full bg-gray-200 flex items-center justify-center text-2xl font-bold rounded shadow backface-hidden">
           ❓
         </div>
